b3183cc696297c379f7a1013e8a9b17f1a50c088
gyu85 <gyu.will85@gmail.com>
2025-01-02 18:26:22 +0900

    [reading] edit 수정 준비 중

--- src/pages/reading/ReadingEditPage.tsx
+++ src/pages/reading/ReadingEditPage.tsx
@@ -1,3 +1,33 @@
-const ReadingEditPage = () => <h3>Edit page</h3>;
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { useIDb, MyReadingsValue } from 'hooks/useIDb';
+
+const ReadingEditPage = () => {
+  const params = useParams();
+  const { db, isLoading, getMyReading } = useIDb();
+  const [myReading, setMyReading] = useState<MyReadingsValue | null>(null);
+
+  useEffect(() => {
+    const { id } = params;
+    if (!isLoading && db) {
+      getMyReading(Number(id)).then((data) => {
+        console.log(data);
+        setMyReading(data);
+      });
+    }
+    console.log(params);
+  }, [params, db]);
+
+  return (
+    <>
+      {myReading && (
+        <section>
+          <h3>책 제목: {myReading.bookInfo.title}</h3>
+        </section>
+      )}
+    </>
+  );
+};
 
 export default ReadingEditPage;
